ef3bf4918f929b3431f5a6b41473433302891ad8
Rutvika <katariarutvika@gmail.com>
2024-12-22 20:09:08 +0530

    Added JWT authentication

--- api/auth.js
+++ api/auth.js
@@ -33,7 +33,31 @@ const users = [
     
     // Handle GET request by returning a 405 Method Not Allowed
     else if (req.method === "GET") {
-      return res.status(405).json({ message: "Method Not Allowed" });
+      //return res.status(405).json({ message: "Method Not Allowed" });
+      const { username, password, email } = req.body;
+  
+      if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required." });
+      }
+  
+      const user = users.find(user => user.username === username && user.password === password);
+  
+      if (user) {
+        const expiresInMins = 30;
+        const token = "your-jwt-token";
+  
+        return res.status(200).json({
+          user: {
+            username: user.username,
+            email: email || user.email,
+            gender: user.gender,
+            expiresInMins,
+            token,
+          },
+        });
+      } else {
+        return res.status(401).json({ message: "Invalid username or password" });
+      }
     }
   
     // Handle unsupported methods (like PUT, DELETE) with a 405 status
